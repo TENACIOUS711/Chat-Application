c062351812bdc55f89c141a0cb460072c8389f42
TENACIOUS711 <105707227+TENACIOUS711@users.noreply.github.com>
2024-03-03 04:34:50 +0530

    created reusable editable input

--- src/components/DashBoard/index.js
+++ src/components/DashBoard/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Drawer, Button, Loader } from 'rsuite';
+import { Drawer, Button, Loader, Divider } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInputs';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
   if (!profile) {
     return <Loader center vertical size="md" speed="slow" />;
   }
+  const onSave = async newData => {
+    console.log(newData);
+  };
   return (
     <>
       <Drawer.Header>
@@ -14,6 +18,13 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
