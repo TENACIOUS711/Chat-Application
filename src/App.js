5ebc0273edf6f9846a8b48ad246958be6f03c896
TENACIOUS711 <105707227+TENACIOUS711@users.noreply.github.com>
2024-03-02 17:19:34 +0530

    manage user with profile context and put real time subs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import ProfileProvider from './context/profile.context';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <PublicRoute path="/signin">
-          <SignIn />
-        </PublicRoute>
-        <PrivateRoute path="/">
-          <Home />
-        </PrivateRoute>
-      </Switch>
-    </Router>
+    <ProfileProvider>
+      <Router>
+        <Switch>
+          <PublicRoute path="/signin">
+            <SignIn />
+          </PublicRoute>
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </ProfileProvider>
   );
 }
 
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import { useProfile } from '../context/profile.context';
+import { Container, Loader } from 'rsuite';
 
 const PrivateRoute = ({ children, ...routeProps }) => {
-  const profile = false;
-  if (!profile) {
+  const { profile, isLoading } = useProfile();
+
+  if (isLoading && !profile) {
+    <Container>
+      <Loader center vertical size="md" content="Loading..." speed="slow" />
+    </Container>;
+  }
+  if (!profile && !isLoading) {
     return <Redirect to="/SignIn" />;
   }
   return <Route {...routeProps}>{children}</Route>;
