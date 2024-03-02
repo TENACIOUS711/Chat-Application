1f4dabe3093d949d7ea10f6b479a07640647cc61
TENACIOUS711 <105707227+TENACIOUS711@users.noreply.github.com>
2024-03-02 20:43:38 +0530

    added sign out button and displayed user name

--- src/components/DashBoard/DashBoardToggle.js
+++ src/components/DashBoard/DashBoardToggle.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { Button, Icon, Drawer } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
 import Dashboard from '.';
 import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
+import { auth } from '../../misc/firebase';
 
 const DashBoardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+
+    Alert.info('signed out', 4000);
+    close();
+  }, []);
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
--- src/components/DashBoard/index.js
+++ src/components/DashBoard/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Drawer, Button, Loader } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
-  return <div>Hello</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+  if (!profile) {
+    return <Loader center vertical size="md" speed="slow" />;
+  }
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title></Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign Out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 export default Dashboard;
