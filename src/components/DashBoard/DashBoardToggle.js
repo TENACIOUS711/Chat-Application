1cbc509838032d59c0a9c33e2abd21be6bf1c912
TENACIOUS711 <105707227+TENACIOUS711@users.noreply.github.com>
2024-03-02 19:07:05 +0530

    started building sidebars and dashboards

---
+++ src/components/DashBoard/DashBoardToggle.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Icon, Drawer } from 'rsuite';
+import Dashboard from '.';
+import { useModalState } from '../../misc/custom-hooks';
+
+const DashBoardToggle = () => {
+  const { isOpen, close, open } = useModalState();
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" /> Dashboard
+      </Button>
+      <Drawer show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  );
+};
+export default DashBoardToggle;
---
+++ src/components/SideBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashBoardToggle from './DashBoard/DashBoardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className="h-100 pt-2">
+      <div>
+        <DashBoardToggle />
+      </div>
+      bottom
+    </div>
+  );
+};
+export default Sidebar;
